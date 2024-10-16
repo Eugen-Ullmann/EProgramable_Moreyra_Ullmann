0f943949d1fa0f9ccec999588252b83822f3c182
Eugen-Ullmann <eugen.ullmann@ingenieria.edu.ar>
2024-10-16 08:51:41 -0300

    Proyecto 3 creacion

---
+++ firmware/examples/Proyecto_3/main/Proyecto_3.c
@@ -0,0 +1,37 @@
+/*! @mainpage Project 3
+ *
+ * \section genDesc General Description
+ *
+*.
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 12/09/2023 | Document creation		                         |
+ *
+ * @author Eugen Ullmann (eu.ullmann@gmail.com) & Jesus Moreyra (jesus.moreyra@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "switch.h"
+/*==================[macros and definitions]=================================*/
+#define CONFIG_BLINK_PERIOD 150
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+	
+}
+	
+/*==================[end of file]============================================*/
