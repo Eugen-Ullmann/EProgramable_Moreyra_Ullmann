8e747fbd5660d734dd4a5af3d2bfc2962c69bcec
Eugen-Ullmann <eugen.ullmann@ingenieria.edu.ar>
2024-10-16 11:01:34 -0300

    Codigo del controlador del rele y el sensor hechos.

--- firmware/examples/Proyecto_3/main/Proyecto_3.c
+++ firmware/examples/Proyecto_3/main/Proyecto_3.c
@@ -2,7 +2,7 @@
  *
  * \section genDesc General Description
  *
-*.
+ *.
  *
  * @section changelog Changelog
  *
@@ -20,18 +20,64 @@
 #include <stdbool.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "led.h"
-#include "switch.h"
+#include "analog_io_mcu.h"
+#include "uart_mcu.h"
+#include "freertos/task.h"
+#include "timer_mcu.h"
+#include "gpio_mcu.h"
 /*==================[macros and definitions]=================================*/
 #define CONFIG_BLINK_PERIOD 150
+#define GPIO_RELE GPIO_1
 /*==================[internal data definition]===============================*/
+uint32_t threshold = 2400;
 
 /*==================[internal functions declaration]=========================*/
 
 /*==================[external functions definition]==========================*/
 void app_main(void)
 {
-	
+    analog_input_config_t config;
+
+    config.input = CH0;
+    config.mode = ADC_SINGLE;
+    config.func_p = NULL;
+    config.param_p = NULL;
+    config.sample_frec = NULL;
+
+    AnalogInputInit(&config);
+    uint16_t valorLectura = 0;
+
+    serial_config_t my_uart = {
+        .port = UART_PC,
+        .baud_rate = 115200,
+        .func_p = NULL,
+        .param_p = NULL};
+
+    UartInit(&my_uart);
+
+    GPIOInit(GPIO_RELE, GPIO_OUTPUT);
+    GPIOOff(GPIO_RELE);
+    GPIOOn(GPIO_RELE);
+
+    while (true)
+    {
+
+        AnalogInputReadSingle(CH0, &valorLectura);
+        UartSendString(UART_PC, (char *)UartItoa(valorLectura, 10));
+        UartSendString(UART_PC, "\r\n");
+
+        if (valorLectura > threshold)
+        {
+            GPIOOff(GPIO_RELE);
+        }
+        else
+        {
+            GPIOOn(GPIO_RELE);
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    
 }
-	
+
+
 /*==================[end of file]============================================*/
